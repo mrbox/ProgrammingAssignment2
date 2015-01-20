35d38262daa4ec59751b28eae0b95fe813683605
Jakub Paczkowski <jakub@paczkowski.eu>
2015-01-20 03:24:12 +0100

    Programming Assignment 2, caching inversion of matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions in this file are used to efficiently compute inversion of a given matrix. 
+## If inversion have already been calculated, return cached value
 
-## Write a short comment describing this function
+## Create list of functions giving access to original matrix and to it's inverted version cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Perforcm check if inversion of matrix x has been already computed and if so return inversion. 
+## If not, compute inversion, store it in cache and return
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
+  ## Return a matrix that is the inverse of 'x'
 }
